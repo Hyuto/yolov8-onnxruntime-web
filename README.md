5e0bb7654277d4c0d24810a135c205b725ab5fac
Wahyu Setianto <wahyusetianto19@gmail.com>
2023-01-15 19:58:28 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,85 @@
+# YOLOv8 with onnxruntime-web
+
+<p align="center">
+  <img src="./sample.png" />
+</p>
+
+![love](https://img.shields.io/badge/Made%20with-🖤-white)
+![react](https://img.shields.io/badge/React-blue?logo=react)
+![onnxruntime-web](https://img.shields.io/badge/onnxruntime--web-white?logo=onnx&logoColor=black)
+![opencv.js](https://img.shields.io/badge/opencv.js-green?logo=opencv)
+
+---
+
+Object Detection application right in your browser.
+Serving YOLOv8 in browser using onnxruntime-web with `wasm` backend.
+
+## Setup
+
+```bash
+git clone https://github.com/Hyuto/yolov8-onnxruntime-web.git
+cd yolov8-onnxruntime-web
+yarn install # Install dependencies
+```
+
+## Scripts
+
+```bash
+yarn start # Start dev server
+yarn build # Build for productions
+```
+
+## Models
+
+**Main Model**
+
+YOLOv8n model converted to onnx.
+
+```
+used model : yolov8n.onnx
+size       : 13 Mb
+```
+
+**NMS**
+
+ONNX model to perform NMS operator [CUSTOM].
+
+```
+nms-yolov8.onnx
+```
+
+## Use another model
+
+> :warning: **Size Overload** : used YOLOv8n model in this repo is the smallest with size of 13 MB, so other models is definitely bigger than this which can cause memory problems on browser.
+
+Use another YOLOv8 model.
+
+1. Export YOLOv8 model to onnx format. Read more on the [official documentation](https://docs.ultralytics.com/tasks/detection/#export)
+
+   ```python
+   from ultralytics import YOLO
+
+   # Load a model
+   model = YOLO("yolov8n.pt")  # load an official model
+
+   # Export the model
+   model.export(format="onnx")
+   ```
+
+2. Copy `yolov8*.onnx` to `./public/model`
+3. Update `modelName` in `App.jsx` to new model name
+   ```jsx
+   ...
+   // configs
+   const modelName = "yolov8*.onnx"; // change to new model name
+   const modelInputShape = [1, 3, 640, 640];
+   const topk = 100;
+   const iouThreshold = 0.4;
+   const scoreThreshold = 0.2;
+   ...
+   ```
+4. Done! 😊
+
+## Reference
+
+- https://github.com/ultralytics/ultralytics
---
+++ src/App.js
@@ -0,0 +1,128 @@
+import React, { useState, useRef } from "react";
+import cv from "@techstark/opencv-js";
+import { Tensor, InferenceSession } from "onnxruntime-web";
+import Loader from "./components/loader";
+import { detectImage } from "./utils/detect";
+import "./style/App.css";
+
+const App = () => {
+  const [session, setSession] = useState(null);
+  const [loading, setLoading] = useState("Loading OpenCV.js...");
+  const [image, setImage] = useState(null);
+  const inputImage = useRef(null);
+  const imageRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  // Configs
+  const modelName = "yolov8n.onnx";
+  const modelInputShape = [1, 3, 640, 640];
+  const topk = 100;
+  const iouThreshold = 0.45;
+  const scoreThreshold = 0.2;
+
+  // wait until opencv.js initialized
+  cv["onRuntimeInitialized"] = async () => {
+    // create session
+    setLoading("Loading YOLOv8 model...");
+    const [yolov8, nms] = await Promise.all([
+      InferenceSession.create(`${process.env.PUBLIC_URL}/model/${modelName}`),
+      InferenceSession.create(`${process.env.PUBLIC_URL}/model/nms-yolov8.onnx`),
+    ]);
+
+    // warmup main model
+    setLoading("Warming up model...");
+    const tensor = new Tensor(
+      "float32",
+      new Float32Array(modelInputShape.reduce((a, b) => a * b)),
+      modelInputShape
+    );
+    await yolov8.run({ images: tensor });
+
+    setSession({ net: yolov8, nms: nms });
+    setLoading(null);
+  };
+
+  return (
+    <div className="App">
+      {loading && <Loader>{loading}</Loader>}
+      <div className="header">
+        <h1>YOLOv8 Object Detection App</h1>
+        <p>
+          YOLOv8 object detection application live on browser powered by{" "}
+          <code>onnxruntime-web</code>
+        </p>
+        <p>
+          Serving : <code className="code">{modelName}</code>
+        </p>
+      </div>
+
+      <div className="content">
+        <img
+          ref={imageRef}
+          src="#"
+          alt=""
+          style={{ display: image ? "block" : "none" }}
+          onLoad={() => {
+            detectImage(
+              imageRef.current,
+              canvasRef.current,
+              session,
+              topk,
+              iouThreshold,
+              scoreThreshold,
+              modelInputShape
+            );
+          }}
+        />
+        <canvas
+          id="canvas"
+          width={modelInputShape[2]}
+          height={modelInputShape[3]}
+          ref={canvasRef}
+        />
+      </div>
+
+      <input
+        type="file"
+        ref={inputImage}
+        accept="image/*"
+        style={{ display: "none" }}
+        onChange={(e) => {
+          // handle next image to detect
+          if (image) {
+            URL.revokeObjectURL(image);
+            setImage(null);
+          }
+
+          const url = URL.createObjectURL(e.target.files[0]); // create image url
+          imageRef.current.src = url; // set image source
+          setImage(url);
+        }}
+      />
+      <div className="btn-container">
+        <button
+          onClick={() => {
+            inputImage.current.click();
+          }}
+        >
+          Open local image
+        </button>
+        {image && (
+          /* show close btn when there is image */
+          <button
+            onClick={() => {
+              inputImage.current.value = "";
+              imageRef.current.src = "#";
+              URL.revokeObjectURL(image);
+              setImage(null);
+            }}
+          >
+            Close image
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default App;
